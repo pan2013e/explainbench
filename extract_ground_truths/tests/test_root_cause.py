51dc495e3e68747921993910bd6cace32a60b349
imamnurby <imamnurby@gmail.com>
2025-10-01 13:34:08 +0800

    add new test cases test_extract_deleted_filenames and test_extract_new_filenames

--- extract_ground_truths/tests/test_root_cause.py
+++ extract_ground_truths/tests/test_root_cause.py
@@ -43,6 +43,42 @@ filter_expr = (filter_lhs, OuterRef(filter_rhs.name))
 query = Query(self.model)
 """
 
+sample_diff_2 = """
+diff --git a/django/db/models/fields/__init__.py b/django/db/models/fields/__init__.py
+--- a/django/db/models/fields/__init__.py
++++ b/django/db/models/fields/__init__.py
+@@ -2332,10 +2332,6 @@ def get_db_prep_value(self, value, connection, prepared=False):
+ value = connection.ops.validate_autopk_value(value)
+ return value
+ 
+- def get_prep_value(self, value):
+- from django.db.models.expressions import OuterRef
+- return value if isinstance(value, OuterRef) else super().get_prep_value(value)
+-
+ def contribute_to_class(self, cls, name, **kwargs):
+ assert not cls._meta.auto_field, (
+ "Model %s can't have more than one auto-generated field."
+diff --git a/new_service/api.py b/new_service/api.py
+new file mode 100644
+index 0000000..e69de29
+--- /dev/null
++++ b/new_service/api.py
+@@ -0,0 +1,5 @@
++class ApiClient:
++    def __init__(self, key):
++        self.key = key
++    def fetch_data(self):
++        return {"data": "sample"}
+diff --git a/django/db/models/fields/old_utils.py b/django/db/models/fields/old_utils.py
+deleted file mode 100644
+index 9e6a0c5..0000000
+--- a/django/db/models/fields/old_utils.py
++++ /dev/null
+@@ -1,2 +0,0 @@
+-def old_utility_function():
+-    pass
+"""
+
 def test_extract_modified_files():
     
     expected_values = [
@@ -87,3 +123,25 @@ def test_extract_buggy_function_names():
     expected_values = [("get_db_prep_value", "function"), ("split_exclude", "function"), ("as_sql", "function")]
     buggy_fnames = extract_buggy_function_names(patch_metadata)
     assert set(buggy_fnames) == set(expected_values)
+    
+def test_extract_new_filenames():
+    expected_new_files = ['new_service/api.py']
+    patch_metadata = parse_patch(sample_diff_2)
+    new_files = extract_new_created_filenames(patch_metadata)
+    assert new_files == expected_new_files
+    
+    expected_new_files = []
+    patch_metadata = parse_patch(sample_diff_1)
+    new_files = extract_new_created_filenames(patch_metadata)
+    assert new_files == expected_new_files
+
+def test_extract_deleted_filenames():
+    expected_deleted_files = ['django/db/models/fields/old_utils.py']
+    patch_metadata = parse_patch(sample_diff_2)
+    deleted_files = extract_deleted_filenames(patch_metadata)
+    assert deleted_files == expected_deleted_files
+    
+    expected_deleted_files = []
+    patch_metadata = parse_patch(sample_diff_1)
+    deleted_files = extract_deleted_filenames(patch_metadata)
+    assert deleted_files == expected_deleted_files
