f13949d77db1bb1236d26f2f3151207fd7d783c9
imamnurby <imamnurby@gmail.com>
2025-10-02 14:13:19 +0800

    add count_empty_scopes and count_all_hunks

--- dataset/extract_ground_truths/utils.py
+++ dataset/extract_ground_truths/utils.py
@@ -1,3 +1,5 @@
+from typing import List, Dict
+
 def remove_indentation(input_text: str) -> str:
     """
     Removes leading spaces and tabs from each line in the input text.
@@ -5,3 +7,42 @@ def remove_indentation(input_text: str) -> str:
     lines = input_text.splitlines()
     stripped_lines = [line.lstrip() for line in lines]
     return "\n".join(stripped_lines)
+
+def count_empty_scopes(parsed_patch_data: List[Dict]) -> int:
+    """
+    Counts the number of hunks with an empty scope name in parsed patch data.
+
+    Args:
+        parsed_patch_data (List[Dict]): The list of dictionaries returned
+                                         by the `parse_patch` function.
+
+    Returns:
+        int: The total count of hunks where the scope name is an empty string.
+    """
+    empty_scope_count = 0
+
+    for file_info in parsed_patch_data:
+        for hunk in file_info.get("hunks", []):
+            scope_info = hunk.get("scope", {})
+            scope_name = scope_info.get("name")
+            if not scope_name:
+                empty_scope_count += 1
+
+    return empty_scope_count
+
+def count_all_hunks(parsed_patch_data: List[Dict]) -> int:
+    """
+    Counts the number of hunks in parsed patch data.
+
+    Args:
+        parsed_patch_data (List[Dict]): The list of dictionaries returned
+                                         by the `parse_patch` function.
+
+    Returns:
+        int: The total count of hunks.
+    """
+    scope_count = 0
+
+    for file_info in parsed_patch_data:
+        scope_count += len(file_info.get("hunks", []))
+    return scope_count
