40f85f9218a9fb40bf1f5d22f1e4ccd19c8485a7
imamnurby <imamnurby@gmail.com>
2025-10-13 08:28:03 +0800

    add extra buggy lines

--- dataset/extract_ground_truths/localization/build_jsonl.py
+++ dataset/extract_ground_truths/localization/build_jsonl.py
@@ -6,6 +6,7 @@ from collections import defaultdict
 
 from dataset.extract_ground_truths.localization.get_class_func_names import get_buggy_class_or_fn_names_with_context
 from dataset.extract_ground_truths.localization.get_buggy_file_names import get_buggy_filenames
+from dataset.extract_ground_truths.localization.get_buggy_lines import get_buggy_lines
 
 def build_ground_truth_instances(dataset_dir: str) -> List[Dict[str, Any]]:
     """
@@ -48,6 +49,7 @@ def build_ground_truth_instances(dataset_dir: str) -> List[Dict[str, Any]]:
         }
         record = get_buggy_class_or_fn_names_with_context(record, dataset_dir)
         record = get_buggy_filenames(record)
+        record = get_buggy_lines(record, dataset_dir)
         instances.append(record)
         count += 1
         if count == 10:
