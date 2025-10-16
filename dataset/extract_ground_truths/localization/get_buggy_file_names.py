8a12274761691c7e3760b9dc89838b9b44b5890c
imamnurby <imamnurby@gmail.com>
2025-10-16 08:06:41 +0800

    get the buggy file names based on the gumtree actions. if not empty, then there are some modifications

--- dataset/extract_ground_truths/localization/get_buggy_file_names.py
+++ dataset/extract_ground_truths/localization/get_buggy_file_names.py
@@ -1,16 +1,23 @@
+import os
+import json
 from typing import Dict, Any
 
-
-def get_buggy_filenames(record: Dict[str, Any]) -> Dict[str, Any]:
+def get_buggy_filenames(record: Dict[str, Any], dataset_dir: str) -> Dict[str, Any]:
     """
     Get the filenames from the buggy function names
     """
-    
-    buggy_items = record.get("buggy_function_names", [])
-    output = set([])
-    for item in buggy_items:
-        parts = item.split("::", 1)
-        buggy_filename = parts[0]
-        output.add(buggy_filename)
-    record["buggy_file_names"] = list(output)
+    gumtree_files = record.get("gumtree_files", [])
+    buggy_filenames = []
+    for path in gumtree_files:
+        full_path = os.path.join(dataset_dir, path) 
+        with open(full_path, "r") as f:
+            gumtree = json.load(f) 
+            actions = gumtree.get("actions", [])
+            if actions:
+                path = path.split("/")[1:]
+                path = "/".join(path)
+                path = path[:-5]
+                path += ".py"
+                buggy_filenames.append(path)
+    record["buggy_file_names"] = buggy_filenames
     return record
