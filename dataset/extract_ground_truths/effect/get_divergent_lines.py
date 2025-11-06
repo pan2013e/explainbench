b1d269fd9d6f21dc6910b5a26422651a699a3576
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-11-07 03:16:50 +0800

    apply function match

--- dataset/extract_ground_truths/effect/get_divergent_lines.py
+++ dataset/extract_ground_truths/effect/get_divergent_lines.py
@@ -3,15 +3,17 @@ from pathlib import Path
 from dataset.extract_ground_truths.effect.trace_util import (
     Traces,
     diff_events,
-    lcs_event_match,
+    event_match,
+    function_match,
     apply_trace_filters
 )
 from dataset.extract_ground_truths.effect.process_agent_patch import (
     get_diff_info_per_instance
 )
 from execution.util import get_fail_to_pass_tests
+from tracer.serializer import serialize
 
-BASE = "/home/yusuf/explainbench/logs/run_evaluation/validate-gold.1020/gold"
+BASE = "/home/zhiyuan/explainbench/logs/run_evaluation/trace.validate-gold.1021/gold"
 
 def load_trace_pair(instance_id, diff_lines, test_id=0):
     # test_id refers to the index of FAIL_TO_PASS tests
@@ -29,10 +31,21 @@ def main(instance_id, test_id=0):
     )
     # use the first FAIL_TO_PASS test case, should allow specifying test_id later
     buggy_traces, patched_traces = load_trace_pair(instance_id, diff_lines, test_id)
-    for buggy_block, patched_block in zip(buggy_traces, patched_traces):
+    # for buggy_block in buggy_traces:
+    #     print(buggy_block.function_name)
+    # print("=====")
+    # for patched_block in patched_traces:
+    #     print(patched_block.function_name)
+    # print("=====")
+    # function_match(buggy_traces, patched_traces)
+    # exit(0)
+    for buggy_block, patched_block in function_match(buggy_traces, patched_traces):
         # Before the divergence point, the function call chain should be the same
-        assert buggy_block.function_name == patched_block.function_name
-        for buggy_event, patched_event in lcs_event_match(buggy_block, patched_block):
+        try:
+            assert buggy_block.function_name == patched_block.function_name, f"Function names differ: {buggy_block.function_name} vs {patched_block.function_name}"
+        except AssertionError:
+            breakpoint()
+        for buggy_event, patched_event in event_match(buggy_block, patched_block):
             assert buggy_event.statement == patched_event.statement
             # Exit when event types differ, usually means test exception raised
             if buggy_event.event_type != patched_event.event_type:
@@ -44,14 +57,20 @@ def main(instance_id, test_id=0):
             print(f'+ {patched_event.event_type:<10} {patched_event.statement}')
             filtered_diff = apply_trace_filters(diff, patched_event)            
             if 'seen_variables' in diff.affected_root_keys and filtered_diff:
+                # return {
+                #     'buggy': buggy_event.model_dump(),
+                #     'patched': patched_event.model_dump(),
+                #     'filtered_diff': serialize(filtered_diff),
+                # }
                 print('Diff: ', filtered_diff)
                 # assert patched_block.params == buggy_block.params, f"CHECK: Function parameters differ in prepatch and postpatch, buggy: {buggy_block.params}, patched: {patched_block.params}"
                 # print('Function parameters: ', patched_block.params)
                 if hasattr(patched_event, 'seen_variables'):
-                    print('Buggy Variables: ', buggy_event.deserialized().seen_variables)
+                    print('Buggy Variables: ', buggy_event.seen_variables)
                     print('====')
-                    print('Patched Variables: ', patched_event.deserialized().seen_variables)
-            input("....")
+                    print('Patched Variables: ', patched_event.seen_variables)
+                input('...')
+            # input("....")
         #         is_break = True
         #         break
         # if is_break:
