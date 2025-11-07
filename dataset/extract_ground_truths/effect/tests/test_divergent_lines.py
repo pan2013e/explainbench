c579c32196d4c75dd0c4c69603b84fa9d03f4ecb
imamnurby <imamnurby@gmail.com>
2025-11-07 16:20:26 +0800

    add test cases

---
+++ dataset/extract_ground_truths/effect/tests/test_divergent_lines.py
@@ -0,0 +1,18 @@
+from dataset.extract_ground_truths.effect.get_divergent_lines import main
+from tracer.serializer import serialize
+
+def test_astropy_7166():
+    expected = {'type_changes': {"root['seen_variables']['dct']['bar']['__doc__']": {'old_type': type(None), 'new_type': str, 'old_value': None, 'new_value': 'BAR'}}}
+    line = main("astropy__astropy-7166", 0, True)
+    assert expected == line
+    
+def test_astropy_12907():    
+    expected = {'values_changed': {"root['seen_variables']['cright']['values'][2][1]": {'new_value': 0.0, 'old_value': 1.0}, "root['seen_variables']['cright']['values'][3][0]": {'new_value': 0.0, 'old_value': 1.0}}}
+    line = main("astropy__astropy-12907", 0, True)
+if __name__ == "__main__":
+    try:
+        test_astropy_7166()
+        test_astropy_12907()
+    except:
+        import pdb
+        pdb.post_mortem()
