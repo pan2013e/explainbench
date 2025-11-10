0a58a28d2b71858c3bfb2d9293f4ce4a622001c7
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-11-10 14:00:25 +0800

    support agent patches

--- execution/inspect.py
+++ execution/inspect.py
@@ -1,34 +1,34 @@
 import os
-import sys
 
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 from swebench.harness.run_evaluation import main as run_evaluation_main
 
 from execution.monkey_patch.dataset import monkey_patch_dataset
 from execution.monkey_patch.inspect import monkey_patch_execution
-from execution.util import all_instances, prepare_tracer
+from execution.util import prepare_tracer, get_instance_ids, get_predictions_path
 
 def main(**kwargs):
     monkey_patch_dataset()
     monkey_patch_execution(**kwargs)
     prepare_tracer()
-    for key in ['bp_file', 'pre_bp_line', 'post_bp_line', 'expr', 'pre_count', 'post_count', 'inspector_mode']:
-        kwargs.pop(key, None)
     run_evaluation_main(
         dataset_name="SWE-bench/SWE-bench_Verified",
         split="test",
-        open_file_limit=4096,
-        timeout=3600,
+        instance_ids=get_instance_ids(kwargs["instance_ids"]),
+        predictions_path=get_predictions_path(kwargs["agent"]),
+        max_workers=kwargs["max_workers"],
         force_rebuild=False,
         cache_level="env",
         clean=False,
+        open_file_limit=4096,
+        run_id=f"inspect.{kwargs["agent"]}.{os.getuid()}",
+        timeout=10800,
         namespace="swebench",
-        instance_image_tag="latest",
-        env_image_tag="latest",
         rewrite_reports=False,
         modal=False,
+        instance_image_tag="latest",
+        env_image_tag="latest",
         report_dir=".",
-        **kwargs
     )
 
 if __name__ == "__main__":
@@ -44,10 +44,9 @@ if __name__ == "__main__":
         help="Instance IDs to run (space separated)",
     )
     parser.add_argument(
-        "-p",
-        "--predictions_path",
+        "--agent",
         type=str,
-        help="Path to predictions file - if 'gold', uses gold predictions",
+        help="Agent submission ID - if 'gold', uses gold predictions",
         required=True,
     )
     parser.add_argument(
@@ -55,9 +54,6 @@ if __name__ == "__main__":
         type=int,
         default=4,
         help="Maximum number of workers (should be <= 75%% of CPU cores)",
-    )
-    parser.add_argument(
-        "-id", "--run_id", type=str, required=True, help="Run ID - identifies the run"
     )
     parser.add_argument(
         "--bp-file", type=str, required=True, help="Path to source file with breakpoint",
@@ -80,16 +76,5 @@ if __name__ == "__main__":
     parser.add_argument(
         "--inspector-mode", type=str, choices=['before', 'after'], default='before',
     )
-    sys.argv = ["swebench.harness.run_evaluation",
-            "--predictions_path", "gold",
-            "--max_workers", "1",
-            "--instance_ids", "astropy__astropy-7166",
-            "--run_id", f"inspect.validate-gold.{os.getuid()}",
-            "--bp-file", "/testbed/astropy/utils/misc.py",
-            "--pre-bp-line", "530",
-            "--pre-count", "10",
-            "--post-bp-line", "526",
-            "--post-count", "10",
-            "--expr", "dct"]
     args = parser.parse_args()
     main(**vars(args))
