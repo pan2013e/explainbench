bc9bb40ff999f4251052d5c43f2ab9dd74a604fa
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-11-10 16:39:49 +0800

    init files

---
+++ dataset/extract_ground_truths/effect/build_step2.py
@@ -0,0 +1,5 @@
+# Build ground truth for effect
+# Step 2. Provide step 1 info to an LLM to infer an expression
+
+if __name__ == "__main__":
+    ...
---
+++ dataset/extract_ground_truths/effect/build_step3.py
@@ -0,0 +1,7 @@
+# Build ground truth for effect
+# Step 3. Run inspector with expressions from step 2 
+# and get the data as ground truth.
+
+
+if __name__ == "__main__":
+    ...
