49139492510c6c84fcae463f69943c7cfc9ab998
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-11-11 20:06:22 +0800

    update step3

--- dataset/extract_ground_truths/effect/build_step3.py
+++ dataset/extract_ground_truths/effect/build_step3.py
@@ -4,8 +4,10 @@
 import os
 import json
 
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
 from execution.inspect import main
-from execution.util import get_instance_ids
+from execution.util import get_instance_ids, get_fail_to_pass_tests
 from dataset.extract_ground_truths.effect.build_step1 import AGENTS, DIR
 
 def read_step2_results():
@@ -13,25 +15,67 @@ def read_step2_results():
         return json.load(f)
 
 def process_agent(data, agent, instance_ids):
+    for instance_id in instance_ids:
+        metadata = data[agent][instance_id]
+        if metadata is None:
+            continue
+        expr = metadata["expr"][0]
+        main([
+            "--instance_id", instance_id,
+            "--agent", agent,
+            "--bp-file", metadata["file_path"],
+            "--pre-bp-line", str(metadata["buggy_lineno"]),
+            "--post-bp-line", str(metadata["patched_lineno"]),
+            "--expr", expr,
+            "--expr-id", "0",
+            "--pre-count", str(metadata["buggy_line_count"]),
+            "--post-count", str(metadata["patched_line_count"]),
+        ])
+
+def process_agent_read_values(data, agent, instance_ids):
     results = {}
     for instance_id in instance_ids:
         metadata = data[agent][instance_id]
-        exprs = metadata["expr"]
-        for expr in exprs:
-            main([
-                "--instance_id", instance_id,
-                "--agent", agent,
-                "--bp-file", metadata["file_path"],
-                "--pre-bp-line", str(metadata["buggy_lineno"]),
-                "--post-bp-line", str(metadata["patched_lineno"]),
-                "--expr", expr,
-                "--pre-count", str(metadata["buggy_line_count"]),
-                "--post-count", str(metadata["patched_line_count"]),
-            ])
+        if metadata is None:
+            continue
+        buggy_value, patched_value = read_inspect_values(agent, instance_id, metadata["test_id"], expr_id=0)
+        results[instance_id] = {
+            "buggy_value": buggy_value,
+            "patched_value": patched_value,
+            **metadata,
+        }
     return results
+
+def read_inspect_values(agent, instance_id, test_id, expr_id=0):
+    log_dir = os.path.join(DIR, f"../../../logs/run_evaluation/inspect.{agent}.{os.getuid()}.{expr_id}/{agent}/{instance_id}")
+    test_name = get_fail_to_pass_tests(instance_id)[test_id]
+    buggy_path = os.path.join(log_dir, f"buggy_traces/{test_name}.jsonl")
+    patched_path = os.path.join(log_dir, f"patched_traces/{test_name}.jsonl")
+    with open(buggy_path, "r") as f:
+        buggy_value = json.load(f)['value']
+    with open(patched_path, "r") as f:
+        patched_value = json.load(f)['value']
+    return buggy_value, patched_value
 
 if __name__ == "__main__":
     step2 = read_step2_results()
-    instance_ids = get_instance_ids("astropy")
-    for agent in AGENTS:
-        process_agent(step2, agent, instance_ids)
+    results = {}
+    instance_ids = get_instance_ids(["astropy__astropy-12907"])
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = {
+            executor.submit(process_agent, step2, agent, instance_ids): agent
+            for agent in AGENTS if agent
+        }
+        for future in as_completed(futures):
+            future.result()
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = {
+            executor.submit(process_agent_read_values, step2, agent, instance_ids): agent
+            for agent in AGENTS if agent
+        }
+        for future in as_completed(futures):
+            agent = futures[future]
+            results[agent] = future.result()
+    with open(os.path.join(DIR, "tmp/step3.json"), "w") as f:
+        json.dump(results, f, indent=2)
+    print("Saved step3 results to tmp/step3.json")
