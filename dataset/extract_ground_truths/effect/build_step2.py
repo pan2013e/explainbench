b3b054ffcd0d13451debb9257e6aea0ed5a03df7
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-11-11 15:19:33 +0800

    update step2 code

--- dataset/extract_ground_truths/effect/build_step2.py
+++ dataset/extract_ground_truths/effect/build_step2.py
@@ -4,6 +4,7 @@ import os
 import json
 
 from tqdm.auto import tqdm
+from functools import lru_cache
 from concurrent.futures import ProcessPoolExecutor, as_completed
 
 from execution.util import get_instance_ids
@@ -14,13 +15,40 @@ from dataset.extract_ground_truths.effect.source_util import get_function_code
 def read_step1_results():
     with open(os.path.join(DIR, "tmp/step1.json"), "r") as f:
         return json.load(f)
-    
+
+@lru_cache
+def read_agent_patch_data(agent):
+    with open(os.path.join(DIR, f"../../explanations/agent_patches/{agent}.json"), "r") as f:
+        return json.load(f)
+
+def build_fn_code(pre_code, post_code):
+    if pre_code == post_code:
+        return pre_code
+    else:
+        return f"# Before Patch:\n{pre_code}\n\n# After Patch:\n{post_code}"
+
+def get_agent_patch(agent, instance_id):
+    data = read_agent_patch_data(agent)
+    patch = data[instance_id]['model_patch'] or None
+    return patch
+
 def process_agent(data, agent, instance_ids):
     results = {}
     for instance_id in instance_ids:
         metadata = data[agent][instance_id]
-        fn_code = get_function_code(instance_id, metadata['file_path'], metadata['function_name'], line_hint=metadata['line_number'])
-        expr_candidates = infer_expression.main(fn_code, metadata["statement"], metadata["buggy_variables"], metadata["patched_variables"])
+        pre_code, post_code = get_function_code(
+            instance_id,
+            metadata['file_path'],
+            metadata['function_name'],
+            patch=get_agent_patch(agent, instance_id),
+            line_hint=(metadata['buggy_lineno'], metadata['patched_lineno']),
+        )
+        expr_candidates = infer_expression.main(
+            build_fn_code(pre_code, post_code),
+            metadata["statement"],
+            metadata["buggy_variables"],
+            metadata["patched_variables"],
+        )
         results[instance_id] = {
             "expr": [expr.expr for expr in expr_candidates],
             **metadata
