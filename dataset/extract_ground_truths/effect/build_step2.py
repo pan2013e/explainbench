1c4ca483b52f364551291db1588b11db7f55104d
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-11-11 15:47:38 +0800

    update step2

--- dataset/extract_ground_truths/effect/build_step2.py
+++ dataset/extract_ground_truths/effect/build_step2.py
@@ -32,20 +32,32 @@ def get_agent_patch(agent, instance_id):
     patch = data[instance_id]['model_patch'] or None
     return patch
 
+def get_simple_function_name(metadata):
+    name = metadata['function_name']
+    if ":" in name:
+        name = name.split(":")[-1]
+    if "." in name:
+        name = name.split(".")[-1]
+    return name
+
 def process_agent(data, agent, instance_ids):
     results = {}
     for instance_id in instance_ids:
         metadata = data[agent][instance_id]
+        if metadata is None:
+            results[instance_id] = None
+            continue
         pre_code, post_code = get_function_code(
             instance_id,
             metadata['file_path'],
-            metadata['function_name'],
+            get_simple_function_name(metadata),
             patch=get_agent_patch(agent, instance_id),
             line_hint=(metadata['buggy_lineno'], metadata['patched_lineno']),
         )
         expr_candidates = infer_expression.main(
             build_fn_code(pre_code, post_code),
             metadata["statement"],
+            metadata["filtered_diff"],
             metadata["buggy_variables"],
             metadata["patched_variables"],
         )
@@ -59,14 +71,17 @@ if __name__ == "__main__":
     step1 = read_step1_results()
     results = {}
     instance_ids = get_instance_ids("astropy")
-    with ProcessPoolExecutor(max_workers=10) as executor:
-        futures = {
-            executor.submit(process_agent, step1, agent, instance_ids): agent
-            for agent in AGENTS if agent
-        }
-        for future in tqdm(as_completed(futures), total=len(futures)):
-            agent = futures[future]
-            results[agent] = future.result()
-    with open(os.path.join(DIR, "tmp/step2.json"), "w") as f:
-        json.dump(results, f, indent=2)
-    print("Saved step2 results to tmp/step2.json")
+    for agent in AGENTS:
+        result = process_agent(step1, agent, instance_ids)
+        results[agent] = result
+    # with ProcessPoolExecutor(max_workers=10) as executor:
+    #     futures = {
+    #         executor.submit(process_agent, step1, agent, instance_ids): agent
+    #         for agent in AGENTS if agent
+    #     }
+    #     for future in tqdm(as_completed(futures), total=len(futures)):
+    #         agent = futures[future]
+    #         results[agent] = future.result()
+    # with open(os.path.join(DIR, "tmp/step2.json"), "w") as f:
+    #     json.dump(results, f, indent=2)
+    # print("Saved step2 results to tmp/step2.json")
