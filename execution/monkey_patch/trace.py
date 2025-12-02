3a049a0ced8bfaa61736283677128894b3afe845
imamnurby <imamnurby@gmail.com>
2025-12-02 11:36:27 +0800

    update path to shared logs

--- execution/monkey_patch/trace.py
+++ execution/monkey_patch/trace.py
@@ -23,7 +23,7 @@ DIR = os.path.dirname(os.path.abspath(__file__))
 GLOBAL_ARGS = dict()
 
 def get_allowed_functions(agent, instance_id):
-    dataset_file = os.path.join(DIR, "../../dataset/extract_ground_truths/effect/allowed_functions.json")
+    dataset_file = "/home/yusuf/explainbench/shared_logs/allowed_functions.json"
     if not os.path.exists(dataset_file):
         print(f"Warning: Allowed functions dataset file not found at {dataset_file}")
         return 'none'
