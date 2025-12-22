2e7f33354a20bb9e961d41c3a7cb06fed3235fd4
imamnurby <imamnurby@gmail.com>
2025-12-22 15:19:55 +0800

    only include Line event

--- dataset/extract_ground_truths/effect/get_divergent_lines.py
+++ dataset/extract_ground_truths/effect/get_divergent_lines.py
@@ -95,7 +95,7 @@ def get_common_lines(function_block: FunctionBlock):
     statements = []
     line_nums = []
     for current_event in events:
-        if not current_event.excluded:
+        if not current_event.excluded and current_event.event_type == "Line":
             statements.append(current_event.statement)
             line_nums.append(current_event.line_number)
     return statements, line_nums
