1ca0a76385878ad937a3bcd907f757af5de5a36c
Zhiyuan Pan <zy_pan@zju.edu.cn>
2025-12-24 16:59:09 +0800

    further lower worker num

--- dataset/extract_ground_truths/effect/build_step2.py
+++ dataset/extract_ground_truths/effect/build_step2.py
@@ -147,7 +147,7 @@ def process_agent(agent_data, agent, instance_ids, n_output):
             )
             return None
     
-    with ThreadPoolExecutor(max_workers=20//min(10, len(AGENTS))) as executor:
+    with ThreadPoolExecutor(max_workers=2) as executor:
         futures = {
             executor.submit(process_instance, instance_id): instance_id
             for instance_id in instance_ids
