35989db2a7cbe955d80b44d95703e73e396ecd50
Zhiyuan Pan <zy_pan@zju.edu.cn>
2026-01-08 04:18:36 +0800

    update agents and timeout; remove verbose log; empty patch => fallback to gold

--- dataset/extract_ground_truths/effect/build_step1.py
+++ dataset/extract_ground_truths/effect/build_step1.py
@@ -15,10 +15,13 @@ from execution.util import get_instance_ids
 from tracer.serializer import serialize
 
 EXTRA_LONG_TIMEOUT = {
-    'django__django-16667': 600,
-    'pylint-dev__pylint-7080': 600,
-    'sympy__sympy-21379': 600,
     'django__django-14500': 600,
+    'django__django-16667': 600,
+    'pylint-dev__pylint-6903': 600,
+    'pylint-dev__pylint-7080': 600,
+    'sphinx-doc__sphinx-9230': 600,
+    'sympy__sympy-17655': 600,
+    'sympy__sympy-21379': 1200,
 }
 
 def _process_instance(instance_id, agent, total_choices, depth_threshold, timeout=300):
@@ -38,14 +41,9 @@ def _process_instance(instance_id, agent, total_choices, depth_threshold, timeou
                     depth_threshold=depth_threshold
                 )
             except IndexError:
-                result = {}
+                result = {} # fallback to gold
                 break
             except AssertionError as e:
-                print(
-                    f"AssertionError for {instance_id} "
-                    f"(agent={agent}, test_id={test_id}): {e}",
-                    flush=True,
-                )
                 test_id += 1
                 continue
             if result:
@@ -53,13 +51,14 @@ def _process_instance(instance_id, agent, total_choices, depth_threshold, timeou
             test_id += 1
         return serialize(result)
     except FileNotFoundError:
-        print(f"FileNotFoundError for {instance_id} with agent {agent}", flush=True)
-        return None
+        return {} # fallback to gold
     except TimeoutError:
         print(f"Timeout for {instance_id} with agent {agent}", flush=True)
         return None
     except Exception as e:
+        import traceback
         print(f"Error for {instance_id} with agent {agent}: {e}", flush=True)
+        traceback.print_exc()
         return None
     finally:
         signal.alarm(0)
@@ -81,14 +80,14 @@ def process_agent(agent, instance_ids, total_choices, depth_threshold, max_worke
 if __name__ == "__main__":
     # ------------ SCRIPT PARAMETERS ------------ #
     AGENTS = [
-        # "20250603_Refact_Agent_claude-4-sonnet",
-        # "20250720_Lingxi-v1.5_claude-4-sonnet-20250514",
-        # "20250805_openhands-Qwen3-Coder-480B-A35B-Instruct",
-        # "20250928_trae_doubao_seed_code",
-        # "20250807_mini-v1.7.0_gpt-5-mini"
+        "20250603_Refact_Agent_claude-4-sonnet",
+        "20250720_Lingxi-v1.5_claude-4-sonnet-20250514",
+        "20250805_openhands-Qwen3-Coder-480B-A35B-Instruct",
+        "20250928_trae_doubao_seed_code",
+        "20250807_mini-v1.7.0_gpt-5-mini",
         "gold",
     ]
-    OUTPUT_DIR = os.path.join("/home/yusuf/explainbench/shared_logs/logs/run_evaluation/output_per_step", f"step1.gold.json")
+    OUTPUT_DIR = os.path.join("/home/yusuf/explainbench/shared_logs/logs/run_evaluation/output_per_step", f"step1.json")
     TOTAL_CHOICES = 4
     DEPTH_THRESHOLD = 3
     # ------------------------------------------- #
