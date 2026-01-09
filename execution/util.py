c035dcbbe488880600abed99eeb65d8b41161324
Zhiyuan Pan <zy_pan@zju.edu.cn>
2026-01-09 23:55:22 +0800

    add excluded instance

--- execution/util.py
+++ execution/util.py
@@ -213,6 +213,7 @@ EXCLUDED_IDS = [
     # (Based on developer patches)
     "django__django-13279",
     "django__django-13551",
+    "django__django-15315",
     "django__django-16801",
     "pytest-dev__pytest-7236",
     "pytest-dev__pytest-7432",
